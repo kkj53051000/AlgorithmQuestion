cba6fcd87f0620f788a9c6c74df55c1a4a11a4ee
kkj53051000 <tkarnrwl7418@naver.com>
2022-06-19 11:51:26 +0900

    add Link

--- BaekJoon/b10828.py
+++ BaekJoon/b10828.py
@@ -1,3 +1,5 @@
+# https://www.acmicpc.net/problem/10828
+
 n = int(input())
 
 inputArr = []
--- Programmers/p43165.py
+++ Programmers/p43165.py
@@ -1,3 +1,5 @@
+# https://programmers.co.kr/learn/courses/30/lessons/43165
+
 from collections import deque
 
 def solution(number, target):
