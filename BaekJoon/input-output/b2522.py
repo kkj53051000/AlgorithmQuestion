2a70a4921ba3202e919cc3838a7c2eb37403e187
kkj53051000 <tkarnrwl7418@naver.com>
2022-06-25 16:22:46 +0900

    b2522 SUCCESS

---
+++ BaekJoon/input-output/b2522.py
@@ -0,0 +1,19 @@
+# https://www.acmicpc.net/problem/2522
+
+t = int(input())
+
+for i in range(0, t):
+
+    for j in range(0, (t-i)-1):
+        print(" ", end="")
+    for j in range(i+1, 0, -1):
+        print("*", end="")
+    
+    print("")
+
+for i in range(0, t-1):
+    for j in range(0, i+1):
+        print(" ", end="")
+    for j in range(0, (t-i)-1):
+        print("*", end="")
+    print("")
